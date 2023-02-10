e860997456c3c7674fa3a524473e7e7398dfd6fc
Will Myers <56778753+willmyethewebsiteguy@users.noreply.github.com>
2023-02-10 13:59:31 -0500

    Update sectionLoader.js
    Added Custom Image Loader & SqsSpectficScripts Functions

--- sectionLoader.js
+++ sectionLoader.js
@@ -58,7 +58,6 @@
   function loadScripts() {
     if (!utils.loadScripts.length) return;
     let hasLoaded = [];
-    
     for (let el of utils.loadScripts){
       if (hasLoaded.includes(el.src) || hasLoaded.includes(el.innerHTML)) continue;
       const script = document.createElement('script');
@@ -81,7 +80,6 @@
         hasLoaded.push(el.src)
       }
     }
-
   }
 
   let LoadContent = (function () {    
@@ -96,8 +94,34 @@
 
       if (allow === 'false' || allow === false) return;
       if (!scripts.length) return;
-
       scripts.forEach(el => utils.loadScripts.push(el))
+    }
+    
+    function pushSqsSpecificScripts(instance) {
+      /*Like Background Videos*/
+      let hasBkgVideos = instance.elements.bkgVideos;
+      
+      /*If Background Video*/
+      if (hasBkgVideos.length) {
+        let sqsLoaderScript = document.querySelector('body > [src*="https://static1.squarespace.com/static/vta"]');
+        utils.loadScripts.push(sqsLoaderScript)
+      }
+    }
+    
+    function imageLoader(instance) {
+      //if (!document.body.classList.contains('sqs-edit-mode')) return;
+      let bkgImages = instance.elements.bkgImages;
+      bkgImages.forEach(el => {
+        el.classList.add('wm-image-loaded')
+        let fData = el.dataset.imageFocalPoint.split(',');
+        let focalPoint = {};
+        focalPoint.x = (parseFloat(fData[0]) * 100) + '%';
+        focalPoint.y = (parseFloat(fData[1]) * 100) + '%';
+        el.style.setProperty('--x', focalPoint.x);
+        el.style.setProperty('--y', focalPoint.y);     
+        el.dataset.load = true;
+        el.src = el.dataset.src
+      })
     }
 
     async function buildHTML(instance) {
@@ -111,6 +135,8 @@
       container.insertAdjacentHTML('afterbegin', html);
       loadSquarespaceContent(instance);
       pushScripts(instance);
+      pushSqsSpecificScripts(instance);
+      imageLoader(instance);
       utils.loaded += 1;
       if (utils.loaded == utils.loaders) {
         loadScripts();
@@ -142,6 +168,12 @@
         container: el,
         get scripts() {
           return this.container.querySelectorAll('script');
+        },
+        get bkgVideos() {
+          return this.container.querySelectorAll('.section-background .sqs-video-background-native');
+        },
+        get bkgImages() {
+          return this.container.querySelectorAll('.section-background > img:not(.wm-image-loaded)');
         }
       };
             
